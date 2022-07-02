6e55d5c28804633245548aa6e7c6b53023738129
EC2 Default User <ec2-user@ip-172-31-22-182.ap-northeast-1.compute.internal>
2022-07-02 05:28:31 +0000

    [Finish]aboutページのレイアウト作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :books, only: [:index, :show, :edit]
   root to: 'homes#top'
+  get 'home/about'=>'homes#about'
 end
